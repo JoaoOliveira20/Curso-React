5a4837261b59431dac84c511a78ae689de64e961
JoaoOliveira20 <145066661+JoaoOliveira20@users.noreply.github.com>
2024-04-26 17:54:01 -0300

    eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,30 +1,16 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
-
-  const nome = "Maria"
-
   return (
     <div className="App">
-      <h1>Testando css</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="João"/>
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome={nome} />
-      <Pessoa 
-      nome="Rodrigo" 
-      idade="28" 
-      profissao="Programador" 
-      foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="1" />
+      <Form />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
