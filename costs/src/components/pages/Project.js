5c688c85f65234f2045c86b08998574c6076acfd
JoaoOliveira20 <145066661+JoaoOliveira20@users.noreply.github.com>
2024-08-27 00:12:58 -0300

    edit

---
+++ costs/src/components/pages/Project.js
@@ -0,0 +1,31 @@
+import {useParams} from 'react-router-dom'
+import {useEffect, useState} from 'react'
+
+function Project() {
+
+    const {id} = useParams()
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'Get',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+            .then((data) => {
+                setProjects(data)
+            })
+            .catch(err => console.log(err))
+
+    }, [id])
+
+
+    return (
+        <p>{Project.name}</p>
+    )
+}
+
+export default Project
